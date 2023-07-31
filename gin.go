6c33dfecbf9f8b76602cd9c5b46001416e5e83af
Samir Ettali <ettali.samir@gmail.com>
2023-07-31 15:43:34 +0200

    chore: add x-access-token to allowed cors headers

--- gin.go
+++ gin.go
@@ -30,7 +30,7 @@ func GinLogger() gin.HandlerFunc {
 func GinCors(allowedOrigins ...string) gin.HandlerFunc {
 	return cors.New(cors.Config{
 		AllowMethods:     []string{"GET", "POST", "HEAD", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization", "X-Access-Token"},
 		AllowOrigins:     allowedOrigins,
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
